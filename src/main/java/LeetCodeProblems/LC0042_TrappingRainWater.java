914b9107b9792e2750ed8a81794418ffa4cfc26e
FrankFeng <fqf2009@gmail.com>
2022-06-06 10:51:20 -0700

    DP

--- src/main/java/LeetCodeProblems/LC0042_TrappingRainWater.java
+++ src/main/java/LeetCodeProblems/LC0042_TrappingRainWater.java
@@ -1,5 +1,7 @@
 package LeetCodeProblems;
 
+import java.util.stream.IntStream;
+
 class LC0042_TrappingRainWater {
     public int trap(int[] height) {
         int n = height.length;
@@ -20,5 +22,23 @@ class LC0042_TrappingRainWater {
         }
 
         return res;
+    }
+}
+
+class LC0042_TrappingRainWater1 {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] leftWall = new int[n];
+        int[] rightWall = new int[n];
+        leftWall[0] = height[0];
+        rightWall[n - 1] = height[n - 1];
+
+        for (int i = 1; i < n - 1; i++) {
+            leftWall[i] = Math.max(leftWall[i - 1], height[i]);
+            rightWall[n - 1 - i] = Math.max(rightWall[n - i], height[n - i - 1]);
+        }
+
+        return IntStream.range(0, n).map(i ->
+                Math.max(Math.min(leftWall[i], rightWall[i]) - height[i], 0)).sum();
     }
 }
--- src/test/java/LeetCodeProblems/LC0042_TrappingRainWaterTest.java
+++ src/test/java/LeetCodeProblems/LC0042_TrappingRainWaterTest.java
@@ -1,30 +1,21 @@
 package LeetCodeProblems;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LC0042_TrappingRainWaterTest {
-    private LC0042_TrappingRainWater solution;
-
-    @BeforeEach
-    void setup() {
-        solution = new LC0042_TrappingRainWater();
+    @Test
+    void testCase() {
+        LC0042_TrappingRainWater sol = new LC0042_TrappingRainWater();
+        assertEquals(6, sol.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        assertEquals(9, sol.trap(new int[]{4, 2, 0, 3, 2, 5}));
     }
 
     @Test
     void testCase1() {
-        int rain = solution.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
-        System.out.println(rain);
-        assertEquals(6, rain);
-    }
-
-    @Test
-    void testCase2() {
-        int rain = solution.trap(new int[]{4, 2, 0, 3, 2, 5});
-        System.out.println(rain);
-        assertEquals(9, rain);
-
+        LC0042_TrappingRainWater1 sol = new LC0042_TrappingRainWater1();
+        assertEquals(6, sol.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        assertEquals(9, sol.trap(new int[]{4, 2, 0, 3, 2, 5}));
     }
 }
